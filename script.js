dc1ed18aa2d69f7a5d08889a119b029af496fb3f
edjozero <ej21serafin@gmail.com>
2024-09-30 22:30:01 -0700

    Add starter HTML and create math functions

---
+++ script.js
@@ -0,0 +1,27 @@
+/* 
+FUNCTIONS
+*/
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+/* 
+VARIABLES
+*/
+
+/* 
+LISTENERS 
+*/
